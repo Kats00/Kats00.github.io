2235a727d5382d3c1f3679c84dbbc28088009f3f
Kats00 <ksoohgt3@gmail.com>
2024-03-13 03:25:04 +0800

    update

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
         '"Rubik Doodle Shadow", system-ui',
         '"Patrick Hand SC", cursive'
     ];
-    let currentFontIndex = 0;
+    let currentIndex = 0;
 
-    // Function to handle scrolling of text elements
     function handleScroll(event) {
         const deltaX = event.deltaX;
         const scrollAmount = 20;
@@ -17,10 +16,8 @@ document.addEventListener('DOMContentLoaded', function() {
             const contentWidth = textElement.scrollWidth;
             let currentMarginLeft = parseInt(getComputedStyle(textElement).marginLeft);
             
-            // Update margin left based on scroll direction
             currentMarginLeft += deltaX > 0 ? scrollAmount : -scrollAmount;
             
-            // Check if scrolling exceeds container bounds
             if (currentMarginLeft > 0) {
                 currentMarginLeft = -(contentWidth - containerWidth);
             } else if (currentMarginLeft < -(contentWidth - containerWidth)) {
@@ -28,21 +25,12 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             
             textElement.style.marginLeft = currentMarginLeft + 'px';
-
-            // Change font
-            textElement.style.fontFamily = fonts[currentFontIndex];
         });
 
-        // Cycle through fonts
-        currentFontIndex = (currentFontIndex + 1) % fonts.length;
-
-        event.preventDefault();
     }
 
-    // Listen for wheel event on the document
-    document.addEventListener('wheel', handleScroll);
+    document.addEventListener('wheel', handleScroll, { passive: false });
 
-    // Your code for background image changes based on scrolling can go here
     const background = document.querySelector('.background');
     const images = [
         '../assets/images/backgrounds/circle-specs.png',
@@ -52,21 +40,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const scrollThreshold = 1500;
     const scrollChange = 100;
-    let currentIndex = 0;
     let lastScrollPosition = 0;
 
     window.addEventListener('scroll', function() {
         const currentScrollPosition = window.scrollY;
 
         if (Math.abs(currentScrollPosition - lastScrollPosition) >= scrollChange) {
-            if (currentScrollPosition > scrollThreshold) {
-                currentIndex = (currentIndex + 1) % images.length;
-                background.style.background = `url(${images[currentIndex]}), var(--offwhite)`;
-            } else {
-                currentIndex = (currentIndex - 1 + images.length) % images.length;
-                background.style.background = `url(${images[currentIndex]}), var(--offwhite)`;
-            }
+            const currentIndexShift = currentScrollPosition > scrollThreshold ? 1 : -1;
+            currentIndex = (currentIndex + currentIndexShift + images.length) % images.length;
+            scrollingTextElements.forEach(textElement => {
+                textElement.style.fontFamily = fonts[currentIndex];
+            });
+            background.style.background = `url(${images[currentIndex]}), var(--offwhite)`;
             lastScrollPosition = currentScrollPosition;
         }
     });
+
+    var swiper = new Swiper(".mySwiper", {
+        direction: "vertical",
+        slidesPerView: 1,
+        spaceBetween: 30,
+        mousewheel: true,
+        grabCursor: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
 });
